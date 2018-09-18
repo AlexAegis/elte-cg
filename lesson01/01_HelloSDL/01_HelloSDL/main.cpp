3223cb9d41d0ddb3104649aa7276be15fa4f1d1a
Győri Sándor <alexaegis@gmail.com>
2018-09-18 09:27:10 +0200

    sdf

--- lesson01/01_HelloSDL/01_HelloSDL/main.cpp
+++ lesson01/01_HelloSDL/01_HelloSDL/main.cpp
@@ -1,5 +1,7 @@
 #include <SDL.h>
 #include <iostream>
+#include <string>
+#include <vector>
 
 void exitProgram()
 {
@@ -7,8 +9,60 @@ void exitProgram()
 	std::cin.get();
 }
 
+void drawLetter(SDL_Renderer* ren, int pos, std::string letter) {
+	int offset = pos * 35;
+
+	if (letter == "H" || letter == "E"  || letter == "L" || letter == "O") {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 10, // vonal kezdõpontjának (x,y) koordinátái
+			10 + offset, 40);
+	}
+
+	if (letter == "H" || letter == "E" || letter == "L" || letter == "O") {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 40, // vonal kezdõpontjának (x,y) koordinátái
+			10 + offset, 70);
+	}
+
+	if (letter == "E" || letter == "O") {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 10, // vonal kezdõpontjának (x,y) koordinátái
+			40 + offset, 10);
+	}
+
+
+	if (letter == "H" || letter == "E") {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 40, // vonal kezdõpontjának (x,y) koordinátái
+			40 + offset, 40);
+	}
+
+
+	if (letter == "E" || letter == "L" || letter == "O") {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 70, // vonal kezdõpontjának (x,y) koordinátái
+			40 + offset, 70);
+	}
+
+
+	if (letter == "H" || letter == "O") {
+		SDL_RenderDrawLine(ren,
+			40 + offset, 10, // vonal kezdõpontjának (x,y) koordinátái
+			40 + offset, 40);
+	}
+
+
+	if (letter == "H" || letter == "O") {
+		SDL_RenderDrawLine(ren,
+			40 + offset, 40, // vonal kezdõpontjának (x,y) koordinátái
+			40 + offset, 70);
+	}
+
+}
+
 int main( int argc, char* args[] )
 {
+
 	// állítsuk be, hogy kilépés elõtt hívja meg a rendszer az exitProgram() függvényt
 	atexit( exitProgram );
 
@@ -78,27 +132,51 @@ int main( int argc, char* args[] )
 							0,		// kék
 							255);	// átlátszatlanság
 
-	SDL_RenderDrawLine(	ren,	// renderer címe, ahová vonalat akarunk rajzolni
-						10, 10, // vonal kezdõpontjának (x,y) koordinátái
-						10, 60);// vonal végpontjának (x,y) koordinátái
 
-	// 1. feladat: egészítsük ki a fenti vonalat egy H betûvé!
 
-	// ...
 
-	// 2. feladat: írjuk ki a "HELLO" szöveget a képernyõre! Ehhez használható a
-	// SDL_RenderDrawLines( <renderer ptr>, <SDL_Point tömb>, <pontok száma>); parancs!
-
-	// ...
 
 	// 3. feladat: 2 másodpercenként változzon a háttér színe! Elõször legyen piros, aztán zöld és végül kék,
 	// majd lépjen ki a program!
+	std::vector<int> currColor{ 255, 0, 0 };
+	while (true) {
+		// aktuális rajzolási szín legyen fekete és töröljük az aktuális rajzolási színnel az ablak kliensterületét
+		SDL_SetRenderDrawColor(ren,	// melyik renderelõnek állítjuk be az aktuális rajzolási színét
+			currColor[0],	// vörös intenzitás - 8 bites elõjel nélküli egész szám
+			currColor[1],		// zöld intenzitás - 8 bites elõjel nélküli egész szám
+			currColor[2],		// kék intenzitás - 8 bites elõjel nélküli egész szám
+			255);	// átlátszóság - 8 bites elõjel nélküli egész szám
+		SDL_RenderClear(ren);
 
-	// jelenítsük meg a backbuffer tartalmát
-	SDL_RenderPresent(ren);
+		// aktuális rajzolási szín legyen zöld és rajzoljunk ki egy vonalat
+		SDL_SetRenderDrawColor(ren,	// renderer címe, aminek a rajzolási színét be akarjuk állítani
+			0,		// piros
+			255,	// zöld
+			0,		// kék
+			255);	// átlátszatlanság
 
-	// várjunk 2 másodpercet
-	SDL_Delay(2000);
+
+	// 1. feladat: egészítsük ki a fenti vonalat egy H betûvé!
+
+
+		drawLetter(ren, 1, "H");
+
+		// 2. feladat: írjuk ki a "HELLO" szöveget a képernyõre! Ehhez használható a
+		// SDL_RenderDrawLines( <renderer ptr>, <SDL_Point tömb>, <pontok száma>); parancs!
+
+		drawLetter(ren, 2, "E");
+		drawLetter(ren, 3, "L");
+		drawLetter(ren, 4, "L");
+		drawLetter(ren, 5, "O");
+
+			// jelenítsük meg a backbuffer tartalmát
+		SDL_RenderPresent(ren);
+
+		// várjunk 2 másodpercet
+		SDL_Delay(500);
+		std::rotate(currColor.begin(), currColor.begin() + 1, currColor.end());
+	}
+
 
 	//
 	// 4. lépés: lépjünk ki
