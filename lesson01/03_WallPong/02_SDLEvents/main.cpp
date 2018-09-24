b14bc17f7216f915835539d60db9504659e09179
Győri Sándor <alexaegis@gmail.com>
2018-09-24 22:14:10 +0200

    paddle game scores

--- lesson01/03_WallPong/02_SDLEvents/main.cpp
+++ lesson01/03_WallPong/02_SDLEvents/main.cpp
@@ -1,82 +1,172 @@
-#include <SDL.h>
+ï»¿#include <SDL.h>
 #include <iostream>
 #include <algorithm>
+#include <math.h>
+#include <vector>
 
-void exitProgram()
-{
-	std::cout << "Kilépéshez nyomj meg egy billentyût..." << std::endl;
+void exitProgram() {
+	std::cout << "KilÃ©pÃ©shez nyomj meg egy billentyÃ»t..." << std::endl;
 	std::cin.get();
 }
 
-int main( int argc, char* args[] )
-{
-	atexit( exitProgram );
-	//
-	// 1. lépés: inicializáljuk az SDL-t
-	//
+int clamp(int p, int l, int h) {
+	return std::min(h, std::max(l, p));
+}
 
-	// a grafikus alrendszert kapcsoljuk csak be, ha gond van, akkor jelezzük és lépjün ki
-	if ( SDL_Init( SDL_INIT_VIDEO ) == -1 )
-	{
-		// irjuk ki a hibat es terminaljon a program
-		std::cout << "[SDL indítása]Hiba az SDL inicializálása közben: " << SDL_GetError() << std::endl;
+bool between(int p, int l, int h) {
+	return clamp(p, l, h) == p;
+}
+
+void drawDigit(SDL_Renderer* ren, int pos, int digit) {
+	int offset = pos * 35;
+	int h_offset = 30;
+	if (digit == 4 || digit == 5 || digit == 6 || digit == 8 || digit == 9 || digit == 0) {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 10 + h_offset,
+			10 + offset, 40 + h_offset);
+	}
+
+	if (digit == 2 || digit == 6 || digit == 8 || digit == 0) {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 40 + h_offset,
+			10 + offset, 70 + h_offset);
+	}
+
+	if (digit == 2 || digit == 3 || digit == 5 || digit == 6 || digit == 7 || digit == 8 || digit == 9 || digit == 0) {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 10 + h_offset,
+			40 + offset, 10 + h_offset);
+	}
+
+
+	if (digit == 2 || digit == 3 || digit == 4 || digit == 5 || digit == 6 || digit == 8 || digit == 9) {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 40 + h_offset,
+			40 + offset, 40 + h_offset);
+	}
+
+
+	if (digit == 2 || digit == 3 || digit == 5 || digit == 6 || digit == 8 || digit == 9 || digit == 0) {
+		SDL_RenderDrawLine(ren,
+			10 + offset, 70 + h_offset,
+			40 + offset, 70 + h_offset);
+	}
+
+
+	if (digit == 1 || digit == 2 || digit == 3 || digit == 4 || digit == 7 || digit == 8 || digit == 9 || digit == 0) {
+		SDL_RenderDrawLine(ren,
+			40 + offset, 10 + h_offset,
+			40 + offset, 40 + h_offset);
+	}
+
+
+	if (digit == 1 || digit == 3 || digit == 4 || digit == 5 || digit == 6 || digit == 7 || digit == 8 || digit == 9 || digit == 0) {
+		SDL_RenderDrawLine(ren,
+			40 + offset, 40 + h_offset,
+			40 + offset, 70 + h_offset);
+	}
+
+}
+
+std::vector<int> getdigits(int num) {
+	std::vector<int> res;
+	do {
+		res.push_back(num % 10);
+		num /= 10;
+	} while (num > 0);
+	std::reverse(res.begin(), res.end());
+	return res;
+}
+
+void drawNumber(SDL_Renderer* ren, int number) {
+	std::vector<int> digits = getdigits(number);
+	for (int i = 1; i <= digits.size(); i++) {
+		drawDigit(ren, i, digits.at(i - 1));
+	}
+}
+
+int main( int argc, char* args[] ) {
+	atexit( exitProgram );
+
+	if ( SDL_Init( SDL_INIT_VIDEO ) == -1 ) {
+		std::cout << "[SDL indÃ­tÃ¡sa]Hiba az SDL inicializÃ¡lÃ¡sa kÃ¶zben: " << SDL_GetError() << std::endl;
 		return 1;
 	}
 			
-	//
-	// 2. lépés: hozzuk létre az ablakot, amire rajzolni fogunk
-	//
-
 	SDL_Window *win = 0;
-    win = SDL_CreateWindow( "Paddle Game!",				// az ablak fejléce
-							200,						// az ablak bal-felsõ sarkának kezdeti X koordinátája
-							200,						// az ablak bal-felsõ sarkának kezdeti Y koordinátája
-							640,						// ablak szélessége
-							480,						// és magassága
-							SDL_WINDOW_SHOWN);			// megjelenítési tulajdonságok
+    win = SDL_CreateWindow("Paddle Game!",
+							200,
+							200,
+							640,
+							480,
+							SDL_WINDOW_SHOWN);
 
-	
-
-	// ha nem sikerült létrehozni az ablakot, akkor írjuk ki a hibát, amit kaptunk és lépjünk ki
-    if (win == 0)
-	{
-		std::cout << "[Ablak létrehozása]Hiba az SDL inicializálása közben: " << SDL_GetError() << std::endl;
+    if (win == 0) {
+		std::cout << "[Ablak lÃ©trehozÃ¡sa]Hiba az SDL inicializÃ¡lÃ¡sa kÃ¶zben: " << SDL_GetError() << std::endl;
         return 1;
     }
 
 	SDL_SetWindowResizable(win, SDL_TRUE);
 
     SDL_Renderer *ren = 0;
-    ren = SDL_CreateRenderer(	win, // melyik ablakhoz rendeljük hozzá a renderert
-								-1,  // melyik indexú renderert inicializáljuka
-									 // a -1 a harmadik paraméterben meghatározott igényeinknek megfelelõ elsõ renderelõt jelenti
-								SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);	// az igényeink, azaz
-																						// hardveresen gyorsított és vsync-et beváró
-    if (ren == 0)
-	{
-        std::cout << "[Renderer létrehozása]Hiba az SDL inicializálása közben: " << SDL_GetError() << std::endl;
+    ren = SDL_CreateRenderer(	win,
+								-1,
+								SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == 0) {
+        std::cout << "[Renderer lÃ©trehozÃ¡sa]Hiba az SDL inicializÃ¡lÃ¡sa kÃ¶zben: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-	//
-	// 4. lépés: indítsuk el a fõ üzenetfeldolgozó ciklust
-	// 
-
-	// véget kell-e érjen a program futása?
+	// vÃ©get kell-e Ã©rjen a program futÃ¡sa?
 	bool quit = false;
-	// feldolgozandó üzenet ide kerül
+
+	int w;
+	int h;
+	SDL_GetWindowSize(win, &w, &h);
+
+	// feldolgozandÃ³ Ã¼zenet ide kerÃ¼l
 	SDL_Event ev;
-	// egér X és Y koordinátái
+	// egÃ©r X Ã©s Y koordinÃ¡tÃ¡i
 	Sint32 mouseX = 0, mouseY = 0;
 	Sint32 unit = 20;
+	Sint32 speed = 2;
+	Sint32 credit = 0;
+	Sint32 dirX = 1;
+	Sint32 dirY = 1;
 
-	while (!quit)
-	{
-		// amíg van feldolgozandó üzenet dolgozzuk fel mindet:
-		while ( SDL_PollEvent(&ev) )
-		{
-			switch (ev.type)
-			{
+	SDL_Rect paddle;
+	paddle.w = unit * 4;
+	paddle.h = unit * 1;
+	paddle.x = std::min(w - paddle.w, std::max(0, mouseX - paddle.w / 2));
+	paddle.y = h - (unit * 3);
+
+	SDL_Rect ball;
+	ball.x = (w / 2 - unit / 2);
+	ball.y = (h / 2 - unit / 2);
+	ball.w = unit;
+	ball.h = unit;
+
+	SDL_Rect wall_w;
+	wall_w.x = 0;
+	wall_w.y = 0;
+	wall_w.w = unit;
+	wall_w.h = h;
+
+	SDL_Rect wall_n;
+	wall_n.x = unit;
+	wall_n.y = 0;
+	wall_n.w = w - unit * 2;
+	wall_n.h = unit;
+
+	SDL_Rect wall_e;
+	wall_e.x = w - unit;
+	wall_e.y = 0;
+	wall_e.w = unit;
+	wall_e.h = h;
+
+	while (!quit) {
+		while ( SDL_PollEvent(&ev) ) {
+			switch (ev.type) {
 			case SDL_QUIT:
 				quit = true;
 				break;
@@ -89,56 +179,83 @@ int main( int argc, char* args[] )
 				mouseY = ev.motion.y;
 				break;
 			case SDL_MOUSEBUTTONUP:
-				// egérgomb felengedésének eseménye; a felengedett gomb a ev.button.button -ban található
-				// a lehetséges gombok a következõek: SDL_BUTTON_LEFT, SDL_BUTTON_MIDDLE, 
-				//		SDL_BUTTON_RIGHT, SDL_BUTTON_WHEELUP, SDL_BUTTON_WHEELDOWN
+
 				break;
 			}
 		}
 
-		SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
-		SDL_RenderClear(ren);
-
-		
-		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-
-		int w;
-		int h;
 		SDL_GetWindowSize(win, &w, &h);
 
-		SDL_Rect paddle;
-		paddle.w = unit * 4;
-		paddle.h = unit * 1;
-		paddle.x = std::min(w - paddle.w, std::max(0, mouseX - paddle.w / 2));
+		wall_w.x = 0;
+		wall_w.y = 0;
+		wall_w.w = unit;
+		wall_w.h = h;
+
+		wall_n.x = unit;
+		wall_n.y = 0;
+		wall_n.w = w - unit * 2;
+		wall_n.h = unit;
+
+		wall_e.x = w - unit;
+		wall_e.y = 0;
+		wall_e.w = unit;
+		wall_e.h = h;
+
+
+		paddle.x = clamp(mouseX - paddle.w / 2, wall_w.x + wall_w.w, wall_e.x - paddle.w);
 		paddle.y = h - (unit * 3);
 
-		SDL_RenderFillRect(ren, &paddle);
+		ball.x = ball.x + (dirX * speed);
+		ball.y = ball.y + (dirY * speed);
 
-		SDL_RenderDrawLine(	ren,	// renderer címe, ahová vonalat akarunk rajzolni
-							0, 0, // vonal kezdõpontjának (x,y) koordinátái
-							mouseX, mouseY);// vonal végpontjának (x,y) koordinátái
+		// "collision"
+
+		if (!between(ball.x, wall_w.x + wall_w.w, wall_e.x - ball.w)) {
+			dirX *= -1;
+			credit++;
+		}
+
+		if (std::max(wall_n.y + wall_n.h, ball.y) != ball.y) {
+			dirY *= -1;
+			credit++;
+		}
+
+		if (std::min(h - ball.h, ball.y) != ball.y) {
+			quit = true;
+		}
+
+
+		// "speed guard"
+
+		int originalDirY = dirY;
+
+		if (between(ball.y + ball.h, paddle.y, paddle.y + paddle.h) 
+			&& between(ball.x + ball.w / 2, paddle.x, paddle.x + paddle.w)) {
+			dirY = -1;
+		}
+
+		if (originalDirY != dirY) {
+			speed++;
+			credit++;
+		}
 		
-		// definiáljunk egy (mouseX, mouseY) középpontó, tengelyekkel párhuzamos oldalú
-		// 20x20-as négyzetet:
-		SDL_Rect cursor_rect;
-		cursor_rect.x = mouseX - 10;
-		cursor_rect.y = mouseY - 10;
-		cursor_rect.w = 20;
-		cursor_rect.h = 20;
-		// legyen a kitöltési szín piros
-		SDL_SetRenderDrawColor( ren, 255, 0, 0, 255 );
-		SDL_RenderFillRect( ren, &cursor_rect);
 
-		// 1. feladat: az eltelt idõ függvényében periodikusan nõjjön és csökkenjen
-		//    az egérmutató középpontjával kirajzolt négyszög
+		// "render farm"
+		SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+		SDL_RenderClear(ren);
+		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+		SDL_RenderFillRect(ren, &paddle);
+		SDL_SetRenderDrawColor( ren, 255, 255, 255, 255 );
+		SDL_RenderFillRect( ren, &ball);
+		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+		SDL_RenderFillRect(ren, &wall_w);
+		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+		SDL_RenderFillRect(ren, &wall_n);
+		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+		SDL_RenderFillRect(ren, &wall_e);
 
-		// 2. feladat: ha a user a bal egérgombot nyomja meg akkor a téglalap színe váltson pirosra,
-		//    ha a jobb egérgombot, akkor kékre
+		drawNumber(ren, credit);
 
-		// 3. feladat: rajzolj ki egy 50 sugarú körvonalat az egérmutató köré!
-		// segítség: használd a SDL_RenderDrawLines()-t
-
-		// jelenítsük meg a backbuffer tartalmát
 		SDL_RenderPresent(ren);
 	}
 
